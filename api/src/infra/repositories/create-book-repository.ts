5d6d2a01e5c85a9dbe386ec68fe89ca01082c42f
Rafael Melo <mellowteam@hotmail.com>
2022-06-07 12:53:03 -0300

    prettier, husky and eslint added to he project

--- api/src/infra/repositories/create-book-repository.ts
+++ api/src/infra/repositories/create-book-repository.ts
@@ -1,33 +1,36 @@
-import { Book } from '../../domain/models'
-import { ICreateBookRepository } from '../../domain/repositories/create-book-repository'
-import { DataSourceOperation, UserEntity, BookEntity } from './typeorm'
+import { Book } from "../../domain/models";
+import { ICreateBookRepository } from "../../domain/repositories/create-book-repository";
+import { DataSourceOperation, UserEntity, BookEntity } from "./typeorm";
 
-export class CreateBookRepository implements ICreateBookRepository{
+export class CreateBookRepository implements ICreateBookRepository {
   async create(book: Book): Promise<Book | null> {
-    await DataSourceOperation(async (DataSource)=>{
-      const userRepository = DataSource.getRepository(UserEntity)
-      if(!userRepository){
-        return
+    await DataSourceOperation(async (DataSource) => {
+      const userRepository = DataSource.getRepository(UserEntity);
+      if (!userRepository) {
+        return;
       }
-      let userData = await userRepository.findOne({where: {
+      let userData = await userRepository.findOne({
+        where: {
           id: book.userId,
-      }, relations: ["books"] })
-      if(!userData){
-        return
+        },
+        relations: ["books"],
+      });
+      if (!userData) {
+        return;
       }
-      let bookData = new BookEntity()
-      bookData.date = book.date
-      bookData.description = book.description
-      bookData.title = book.title
-      await DataSource.manager.save(bookData)
-      userData.books.push(bookData)
-      await userRepository.save(userData)
-      book.id = bookData.id
-    })
+      let bookData = new BookEntity();
+      bookData.date = book.date;
+      bookData.description = book.description;
+      bookData.title = book.title;
+      await DataSource.manager.save(bookData);
+      userData.books.push(bookData);
+      await userRepository.save(userData);
+      book.id = bookData.id;
+    });
 
-    if(!book.id){
-      return null
+    if (!book.id) {
+      return null;
     }
-    return book
+    return book;
   }
 }
