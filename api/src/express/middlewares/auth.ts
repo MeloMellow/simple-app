5bd876bf057aac727fe4d55c0747dd56551982ca
Rafael Melo <mellowteam@hotmail.com>
2022-06-07 12:32:24 -0300

    express middleware updated

--- api/src/express/middlewares/auth.ts
+++ api/src/express/middlewares/auth.ts
@@ -3,6 +3,6 @@ import { ExpressMiddlewareAdapter } from '../adapters/express-middleware-adapter
 
 import AuthMiddlewareFactory from '../factories/auth-middleware-factory'
 
-export default async function authMiddleware(req: express.Request, res: express.Response, next: express.NextFunction) {
-  await ExpressMiddlewareAdapter.adapt(AuthMiddlewareFactory.make())(req, res, next)
+export default function authMiddleware(req: express.Request, res: express.Response, next: express.NextFunction) {
+  ExpressMiddlewareAdapter.adapt(AuthMiddlewareFactory.make())(req, res, next)
 }
