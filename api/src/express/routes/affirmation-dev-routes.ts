a59c9b83b3c54c0c639c9fd6d2be20db8b4e52d6
Rafael Melo <mellowteam@hotmail.com>
2022-06-15 12:24:47 -0300

    fix to affrmation/dev endpoint

--- api/src/express/routes/affirmation-dev-routes.ts
+++ api/src/express/routes/affirmation-dev-routes.ts
@@ -4,6 +4,10 @@ import affirmationDevMessage from "../../utils/affirmation-dev-message";
 export default function (router: express.Router) {
   router.get("/affirmation-dev", async (req, res) => {
     const data = await affirmationDevMessage();
-    res.send(data);
+    if (data) {
+      res.send(data);
+    } else {
+      res.status(500).send();
+    }
   });
 }
--- api/src/utils/affirmation-dev-message.ts
+++ api/src/utils/affirmation-dev-message.ts
@@ -1,9 +1,15 @@
 import fetch from "node-fetch";
+import { ServerError } from "../presentation/errors";
 
-export default async function affirmationDevMessage(): Promise<JSON> {
-  const response = await fetch("https://www.affirmations.dev/", {
-    method: "GET",
-  });
-  const data = await response.json();
-  return data;
+export default async function affirmationDevMessage(): Promise<{} | null> {
+  try {
+    const response = await fetch("https://www.affirmations.dev/", {
+      method: "GET",
+    });
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
 }
